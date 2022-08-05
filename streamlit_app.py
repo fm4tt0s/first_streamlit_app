cd2abc7783a8dc86453d52b06fd7abae6e49d138
Felipe Mattos <fstach@gmail.com>
2022-08-05 15:10:17 -0300

    Update streamlit_app.py
    added json rest to fruitvice api

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 # streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
+
+# new session to call out rivers and streamlit
+streamlit.header("Fruityvice Fruit Advice!")
+
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
