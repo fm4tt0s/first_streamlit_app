fa35391f3fd1712509d6c84cfd2e6aa3000ccd60
Felipe Mattos <fstach@gmail.com>
2022-08-05 19:23:54 -0300

    Update streamlit_app.py
    fixed stop typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it in the screen as a table
 streamlit.dataframe(fruityvice_normalized)
-streamlit.stop
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
