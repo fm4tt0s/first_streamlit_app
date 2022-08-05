ae80c2908409f97368b93f5ec3aacb7845425dbe
Felipe Mattos <fstach@gmail.com>
2022-08-05 15:18:34 -0300

    Update streamlit_app.py
    added fruit_choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
@@ -28,9 +29,12 @@ streamlit.dataframe(fruits_to_show)
 # new session to call out rivers and streamlit
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # just writest the json response payload
 # streamlit.text(fruityvice_response.json()) 
 
